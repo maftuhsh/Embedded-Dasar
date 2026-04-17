598af4d20a2b33f44c5ecba6bf08bca6310d36b3
Maftuh Siroj Hamdani <162122749+maftuhsh@users.noreply.github.com>
2026-04-17 16:15:47 +0000

    Add New Module

---
+++ Modul-5/SPI/Master.ino
@@ -0,0 +1,41 @@
+/* program Master */
+#include <SPI.h>
+#define SS 10 //Select Slave
+
+int data = 0;
+boolean state = false;
+
+void setup (void) {
+  Serial.begin(9600);
+  SPI.begin(); //SPI mode Master
+  pinMode(SS, OUTPUT);
+  digitalWrite(SS, HIGH);
+  SPI.setClockDivider(SPI_CLOCK_DIV4); //mengatur clock SPI dari 1/4 frekuensi sistem (frekuensi sistem 16MHz).
+  Serial.println("welcome to module 4");
+  Serial.println("device Master");
+}
+
+void loop (void) {
+  //========= Mengambil nilai dari Serial Monitor ==============
+  if (Serial.available()) {
+    data = Serial.read();
+    data = data - 48;
+    state = true;
+  }
+  //========= Mengirim data ke slave ==============
+  if (state) {
+    int dataFromMISO = sendDataSPI(data);//mengirim nilai dari master, dan mengambil nilai dari slave
+    if (dataFromMISO) Serial.println("data dari slave = " + String (dataFromMISO));
+    else Serial.println("slave gagal ngirim");
+    state = false;
+  }
+  delay(250);
+}
+//======= fungsi write and read data SPI =========
+int sendDataSPI(int _val) {
+  digitalWrite(SS, LOW); //aktifkan pin enable slave
+  int data_receive = SPI.transfer(_val); // mengirim data ke slave, dan mengambil data dari slave
+  digitalWrite(SS, HIGH); // matikan pin enable slave
+  delay(1);
+  return data_receive;
+}
---
+++ Modul-5/SPI/Slave.ino
@@ -0,0 +1,54 @@
+/*program Slave*/
+#include <SPI.h>
+
+#define L1 4 //pin arduino
+#define L2 5 //pin arduino
+#define L3 6 //pin arduino
+#define L4 7 //pin arduino
+#define NUM_LEDS 4 //jumlah led
+
+int leds[NUM_LEDS] = {L1, L2, L3, L4};
+
+volatile uint8_t data = 0; //variable ini disimpan di RAM
+
+void setup (void) {
+  Serial.begin (9600);
+  for (uint8_t indx = 0; indx < NUM_LEDS; indx++) {
+    pinMode(leds[indx], OUTPUT);
+  }
+  pinMode(MISO, OUTPUT); //output untk mengirim data dari slave to master
+  SPCR |= 0b0000001 << 6; //SPI activated
+  SPI.attachInterrupt(); //interrupt activated
+  Serial.println("welcome to module 4");
+  Serial.println("device SLave");
+}
+ISR (SPI_STC_vect) //(Interrupt Service Routine) dipanggil ketika ada interrupt dari SPI
+{
+  data = SPDR; //SPI Data Register
+  SPDR = data;
+}
+
+void loop (void) {
+  Serial.print("data Input = ");
+  Serial.println(data);
+
+  if (data > 0 && data <= NUM_LEDS) {
+    uint8_t bin[NUM_LEDS] = {0b0001, 0b0010, 0b0100, 0b1000};
+    --data;
+    // ============== KONDISI LED ===============
+    for (uint8_t j = 0; j < NUM_LEDS; j++) {
+      uint8_t val = (bin[j] >> data) & 1;
+      Serial.print("LED" + String(j + 1) + " = " + String(val) + " \t");
+      digitalWrite(leds[j], val);
+    }
+    Serial.println();
+    data++;
+  }
+  else {
+    for (uint8_t j = 0; j < NUM_LEDS; j++) {
+      digitalWrite(leds[j], LOW);
+    }
+    Serial.println("all leds off");
+  }
+  delay(250);
+}
